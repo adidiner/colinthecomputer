04a369c1f86a928f30f9589e263da12a8c58d155
Adi Dinerstein <adi.dinerstein@gmail.com>
2019-11-30 15:34:20 +0200

    Created scripts, requirements

---
+++ scripts/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+
+function main {
+    python -m virtualenv .env --prompt "[colin] "
+    find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
+    .env/bin/pip install -U pip
+    .env/bin/pip install -r requirements.txt
+}
+
+
+main "$@"
