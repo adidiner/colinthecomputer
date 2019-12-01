dd9f17ef629839916d7a7314beb229fd70030f46
Adi Dinerstein <adi.dinerstein@gmail.com>
2019-12-01 22:43:43 +0200

    Webserver with flask

--- colin/web.py
+++ colin/web.py
@@ -1,6 +1,6 @@
 import http.server
 import pathlib
-import flask
+from flask import Flask
 
 _INDEX_HTML = '''
 <html>
@@ -36,35 +36,37 @@ _THOUGHT_HTML = '''
 </tr>
 '''
 
-#TODO flask
+website = Flask(__name__)
 
-#@website.route('/')
+@website.route('/')
 def index():
 	data_dir = pathlib.Path(DATA_DIR)
 	users_html = []
 	for user_dir in data_dir.iterdir():
 		users_html.append(_USER_LINE_HTML.format(user_id=user_dir.name))
-	return (200, _INDEX_HTML.format(users='\n'.join(users_html)))
+	return _INDEX_HTML.format(users='\n'.join(users_html)), 200
 
 
-#@website.route('/users/([0-9]+)')
+@website.route('/users/<int:user_id>')
 def user(user_id):
+	user_id = str(user_id)
 	data_dir = pathlib.Path(DATA_DIR)
 	for user_dir in data_dir.iterdir():
 		if user_dir.name == user_id:
 			break
 	else:
-		return (404, '')
+		return '', 404
 
 	thoughts_html = []
 	for thought in user_dir.iterdir():
 		year, hour = thought.stem.split('_')
 		timestamp = '{} {}'.format(year, ':'.join(hour.split('-')))
 		thoughts_html.append(_THOUGHT_HTML.format(timestamp=timestamp, thought=thought.read_text()))
-	return (200, _USER_HTML.format(user_id=user_id, user_thoughts='\n'.join(thoughts_html)))
+	return _USER_HTML.format(user_id=user_id, user_thoughts='\n'.join(thoughts_html)), 200
 
 
 def run_webserver(address, data_dir):
 	global DATA_DIR
 	DATA_DIR = data_dir
-	website.run(address)
+	host, port = address
+	website.run(host=host, port=port)
