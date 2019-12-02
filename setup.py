9e1e7e1ad220310155e912eadaead70a3f342f0e
Adi Dinerstein <adi.dinerstein@gmail.com>
2019-12-02 15:35:13 +0200

    version edit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'colin-the-computer',
-    version = '0.0.1',
+    version = '0.1.0',
     author = 'Adi Dinerstein',
     description = 'Project for Advanced System Design course, TAU.',
     packages = find_packages(),
