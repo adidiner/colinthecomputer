541e6870e59ad8ca1f06e5af7665d7b645380b4f
Adi Dinerstein <adi.dinerstein@gmail.com>
2020-03-04 23:24:16 +0200

    SQL driver

--- colinthecomputer/db_drivers/postgresql_driver.py
+++ colinthecomputer/db_drivers/postgresql_driver.py
@@ -51,7 +51,7 @@ class Feelings(BaseModel):
 
 
 class Snapshot(BaseModel):
-    user = ForeignKeyField(User, backref='snapshots')
+    user_id = IntegerField()
     datetime = DateTimeField()
     pose = ForeignKeyField(Pose, null=True, backref='snapshot')
     color_image = ForeignKeyField(ColorImage, null=True, backref='snapshot')
@@ -61,13 +61,42 @@ class Snapshot(BaseModel):
 
 init_db('127.0.0.1', 5432)
 db.connect()
-db.create_tables([User, Snapshot])
+db.create_tables([User, Snapshot, Translation, Rotation,
+                  Pose, ColorImage, DepthImage, Feelings])
 
-def snapshot_query(user_id, datetime):
-    snapshot_query = (Snapshot.select().where())
+def save_user(user_id, username, birthday, gender):
+    user = User(**locals())
+    user.save()
 
 def save_pose(user_id, datetime, translation, rotation):
     translation = Translation(**translation)
     rotation = Rotation(**rotation)
+    translation.save()
+    rotation.save()
     pose = Pose(translation=translation,
                 rotation=rotation)
+    pose.save()
+    snapshot, _ = Snapshot.get_or_create(user_id=user_id, datetime=datetime)
+    snapshot.pose = pose
+    snapshot.save()
+
+def save_color_image(user_id, datetime, path):
+    color_image = ColorImage(path=path)
+    color_image.save()
+    snapshot, _ = Snapshot.get_or_create(user_id=user_id, datetime=datetime)
+    snapshot.color_image = color_image
+    snapshot.save()
+
+def save_depth_image(user_id, datetime, path):
+    depth_image = DepthImage(path=path)
+    depth_image.save()
+    snapshot, _ = Snapshot.get_or_create(user_id=user_id, datetime=datetime)
+    snapshot.depth_image = depth_image
+    snapshot.save()
+
+def save_feelings(user_id, datetime, feelings):
+    feelings = Feelings(**feelings)
+    feelings.save()
+    snapshot, _ = Snapshot.get_or_create(user_id=user_id, datetime=datetime)
+    snapshot.feelings = feelings
+    snapshot.save()
