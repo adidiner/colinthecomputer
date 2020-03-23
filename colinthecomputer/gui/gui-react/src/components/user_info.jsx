12f69c66c9a0236c3c57f809bb409a03a3b2c8b4
Adi Dinerstein <adi.dinerstein@gmail.com>
2020-03-23 16:17:48 +0200

    user info complete

--- colinthecomputer/gui/gui-react/src/components/user_info.jsx
+++ colinthecomputer/gui/gui-react/src/components/user_info.jsx
@@ -4,6 +4,35 @@ const API_ROOT = "http://127.0.0.1:8000"
 
 class UserInfo extends Component {
   state = {user_info: null}
+
+  renderGender(gender) {
+    switch(gender) {
+      case 'm':
+        return (
+          <span class="badge badge-pill badge-info">male</span>
+          );
+      case 'f':
+        return (
+          <span class="badge badge-pill badge-info">female</span>
+          );
+      case 'o':
+        return (
+          <div style={{display: 'flex',  justifyContent:'center'}}>
+            <span class="badge badge-pill badge-info">gender: other</span>
+          </div>
+          );
+    // todo <span class="bagde badge-pill badge-light">gender</span>
+    }
+  }
+
+  renderBirthday(timestamp) {
+    var date = new Date(timestamp * 1000);
+    var age = new Date(new Date() - date).getFullYear() - 1970;
+    return (
+      <p>{"born " + date.toDateString() + " (age " + age + " years)" }</p>
+      );
+  }
+
   render() {
     if (!this.state.user_info) {
       return (
@@ -23,28 +52,42 @@ class UserInfo extends Component {
 
     var user_info = this.state.user_info;
     return (
-      <div>
-        <div style={{display: 'flex',  justifyContent:'center'}}>
+      <div class='jumbotron' style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>  
+        <div class='col text-center'>
           <h2>{user_info.username}</h2>
+          <h4>{"user #" + user_info.user_id}</h4>
+          {this.renderGender(user_info.gender)}
+          {this.renderBirthday(user_info.birthday)}
+          <button type="button" class="btn btn-outline-dark">view snapshots</button>
         </div>
-        <span class="badge badge-pill badge-info">{user_info.gender}</span>
       </div>
       );
   }
 
   componentDidMount = () => {
-    console.log(this.props)
+    // console.log(this.props);
     var id = this.props.match.params.id;
     fetch(API_ROOT + '/users/' + id, {
       method: 'GET',
       mode:'cors',
       dataType: 'json'
     })
+    .then(response => {
+      if (!response.ok) {
+        console.log(response);
+        throw Error(response.statusText);
+      }
+      return response;
+    })
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log(data);
       this.setState({user_info: data});
+    })
+    .catch(error => {
+      console.log(error); // todo
     });
   }
 }
+
 export default UserInfo;
