5aa134247b8a3b4a591472d3a31a0bc9de0de657
Adi Dinerstein <adi.dinerstein@gmail.com>
2020-04-17 17:57:11 +0300

    trying to get travis to work again

--- tests/test_rabbitmq_driver.py
+++ tests/test_rabbitmq_driver.py
@@ -6,7 +6,7 @@ import colinthecomputer.mq_drivers as drivers
 driver = drivers['rabbitmq']
 # TODO: dont hard code host and port
 _HOST = '127.0.0.1'
-_PORT = 6789
+_PORT = 2222
 
 
 def worker(path, number):
@@ -28,18 +28,20 @@ def consumer(path, topics):
 
 def test_task_queue(tmp_path):
     consumed = tmp_path / 'tasks.txt'
-    driver.task_publish('1', _HOST, _PORT, segment="task_testing")
     p = Process(target=worker, args=(consumed, '1'))
     p.start()
-    time.sleep(2)
+    time.sleep(1)
+    driver.task_publish('1', _HOST, _PORT, segment="task_testing")
+    time.sleep(1)
     p.terminate()
-    driver.task_publish('2', _HOST, _PORT, segment="task_testing")
-    driver.task_publish('3', _HOST, _PORT, segment="task_testing")
     p1 = Process(target=worker, args=(consumed, '2'))
     p2 = Process(target=worker, args=(consumed, '2'))
     p1.start()
     p2.start()
-    time.sleep(2)
+    time.sleep(1)
+    driver.task_publish('2', _HOST, _PORT, segment="task_testing")
+    driver.task_publish('3', _HOST, _PORT, segment="task_testing")
+    time.sleep(1)
     p1.terminate()
     p2.terminate()
     with open(consumed, 'rt') as f:
